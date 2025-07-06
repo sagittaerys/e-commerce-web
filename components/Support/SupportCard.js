349e409b8e3b8a651ec986ee2f8786a799cbefbc
Olamilekan Aremu <aremu0235@gmail.com>
2025-07-06 09:42:11 +0100

    cards-fixed

--- components/Support/SupportCard.js
+++ components/Support/SupportCard.js
@@ -3,14 +3,14 @@
 export default function SupportCard({ icon, cardTitle, cardBody, buttonText, link}) {
   return (
     <div className="px-3 hover:border-[2px] border-[#23263B] shadow-xl mx-auto rounded-md bg-[#91929D	] w-[22rem] py-5">
-      <div className="icons rounded-md text-center p-1 bg-[#23263B] w-[60px] h-[60px]">
+      <div className="icons rounded-md text-center p-1 bg-[#23263B] h-[60px] w-[60px]">
 
         <div className="text-white text-center mx-auto w-1/2 mt-[1rem] text-[1.5rem]">{icon}</div>
       </div>
 
 
       {/* Card text */}
-      <div className="text mb-[1rem] py-2 h-[120px]">
+      <div className="text mb-[1rem] py-2 ">
         <h2 className="mb-[1rem] text-[1.4rem] font-bold text-[#23263B]">{cardTitle}</h2>
         <p className="mb-[1.5rem] text-[#23263B] text-[16px]">{cardBody}</p>
 
